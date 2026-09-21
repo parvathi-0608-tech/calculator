12d8e58337a7a69ce5d740441ef1588aeb0ccb47
Rupavath Parvathi <parvathirupavath28@gmail.com>
2026-09-21 21:53:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let display = document.getElementById("display");
+
+
+function appendValue(value) {
+    display.value += value;
+}
+
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+
+function calculate() {
+
+    try {
+
+        if (display.value === "") {
+            return;
+        }
+
+        display.value = eval(display.value);
+
+    } catch (error) {
+
+        display.value = "Error";
+
+    }
+
+}
